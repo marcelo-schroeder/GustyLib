fa70d4e53425a5b40dbbf57999bbfcef2665e6e4
Marcelo Schroeder <marcelo.schroeder@infoaccent.com>
2014-04-01 07:01:37 +1100

    Starting again... Had some Cocoapods issues

---
+++ GustyLib/GustyLib/GustyLib.h
@@ -0,0 +1,13 @@
+//
+//  GustyLib.h
+//  GustyLib
+//
+//  Created by Marcelo Schroeder on 1/04/2014.
+//  Copyright (c) 2014 InfoAccent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GustyLib : NSObject
+
+@end
---
+++ GustyLib/GustyLib/GustyLib.m
@@ -0,0 +1,13 @@
+//
+//  GustyLib.m
+//  GustyLib
+//
+//  Created by Marcelo Schroeder on 1/04/2014.
+//  Copyright (c) 2014 InfoAccent. All rights reserved.
+//
+
+#import "GustyLib.h"
+
+@implementation GustyLib
+
+@end
