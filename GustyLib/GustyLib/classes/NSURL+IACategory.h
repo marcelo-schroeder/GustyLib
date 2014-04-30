6a78425a1bccc6177a586a30704b336ae6671bb1
Marcelo Schroeder <marcelo.schroeder@infoaccent.com>
2014-04-30 16:57:07 +1000

    Added new category.

---
+++ GustyLib/GustyLib/classes/NSURL+IACategory.h
@@ -0,0 +1,12 @@
+//
+// Created by Marcelo Schroeder on 30/04/2014.
+// Copyright (c) 2014 InfoAccent Pty Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSURL (IACategory)
+
+- (BOOL)m_isAppleUrlScheme;
+
+@end
---
+++ GustyLib/GustyLib/classes/NSURL+IACategory.m
@@ -0,0 +1,26 @@
+//
+// Created by Marcelo Schroeder on 30/04/2014.
+// Copyright (c) 2014 InfoAccent Pty Ltd. All rights reserved.
+//
+
+#import "NSURL+IACategory.h"
+
+
+@implementation NSURL (IACategory)
+
+#pragma mark - Public
+
+- (BOOL)m_isAppleUrlScheme {
+    if (
+            [self.scheme isEqualToString:@"mailto"]
+                    || [self.scheme isEqualToString:@"tel"]
+                    || [self.scheme isEqualToString:@"facetime"]
+                    || [self.scheme isEqualToString:@"sms"]
+            ) {
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
+@end
