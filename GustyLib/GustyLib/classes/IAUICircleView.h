441c168126563770e856b2e5374ec806fab2a8d1
Marcelo Schroeder <marcelo.schroeder@infoaccent.com>
2014-04-30 10:18:10 +1000

    New class added.

---
+++ GustyLib/GustyLib/classes/IAUICircleView.h
@@ -0,0 +1,12 @@
+//
+// Created by Marcelo Schroeder on 30/04/2014.
+// Copyright (c) 2014 InfoAccent Pty Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "IAUIView.h"
+
+
+@interface IAUICircleView : IAUIView
+@property (nonatomic, strong) UIColor *p_foregroundColor;
+@end
---
+++ GustyLib/GustyLib/classes/IAUICircleView.m
@@ -0,0 +1,53 @@
+//
+// Created by Marcelo Schroeder on 30/04/2014.
+// Copyright (c) 2014 InfoAccent Pty Ltd. All rights reserved.
+//
+
+#import "IAUICircleView.h"
+
+
+@implementation IAUICircleView {
+
+}
+
+#pragma mark - Overrides
+
+- (void)m_commonInit {
+    self.backgroundColor = [UIColor clearColor];
+}
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        [self m_commonInit];
+    }
+    return self;
+}
+
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self m_commonInit];
+    }
+   return self;
+}
+
+- (id)initWithCoder:(NSCoder *)coder {
+    self = [super initWithCoder:coder];
+    if (self) {
+        [self m_commonInit];
+    }
+    return self;
+}
+
+- (void)drawRect:(CGRect)rect {
+    [super drawRect:rect];
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    CGContextAddEllipseInRect(ctx, rect);
+    UIColor *l_color = self.p_foregroundColor;
+    l_color = l_color?:[UIColor redColor];
+    CGContextSetFillColor(ctx, CGColorGetComponents([l_color CGColor]));
+    CGContextFillPath(ctx);
+}
+
+@end
