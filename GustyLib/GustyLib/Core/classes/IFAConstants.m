1d35addfe723fec4113842c105b55f86e7a23fd0
Marcelo Schroeder <marcelo.schroeder@infoaccent.com>
2014-12-03 16:54:29 +1100

    Removed todo.

--- GustyLib/GustyLib/Core/classes/IFAConstants.m
+++ GustyLib/GustyLib/Core/classes/IFAConstants.m
@@ -22,7 +22,6 @@
 
 @implementation IFAConstants
 
-//wip: do the same with the others? (for swift)
 CGFloat const IFAMinimumTapAreaDimension = 44;
 CGFloat const IFATableViewEditingCellXOffset = 38;
 CGFloat const IFAFormSectionHeaderDefaultHeight = 39;
