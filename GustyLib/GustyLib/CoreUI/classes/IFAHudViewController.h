4da1df575f5ae40c213bf56fa95b15738cf6de51
Marcelo Schroeder <marcelo.schroeder@infoaccent.com>
2015-02-10 07:10:33 +1100

    Progress with HUD related documentation.

--- GustyLib/GustyLib/CoreUI/classes/IFAHudViewController.h
+++ GustyLib/GustyLib/CoreUI/classes/IFAHudViewController.h
@@ -27,6 +27,9 @@
 * HUD (Heads Up Display) style view controller. It uses <IFAHudView> as the underlying HUD view.
 *
 * All HUD properties can be set here with the exception of appearance related properties, which must be set in the <hudView> property instance directly.
+*
+* Message <presentHudViewControllerWithParentViewController:parentView:animated:completion:> to present the view controller, and
+* <dismissHudViewControllerWithAnimated:completion:> to dismiss it.
 */
 @interface IFAHudViewController : IFAViewController
 
@@ -70,8 +73,13 @@
 * Setting this to YES, automatically sets the <modal> property to YES, which enables user interactivity in the HUD.
 */
 @property (nonatomic) BOOL shouldDismissOnChromeTap;
-@property (nonatomic, strong) void (^chromeTapActionBlock) ();
 
+/**
+* Block to execute when the user taps the HUD's chrome view.
+*
+* Setting this to a value other than nil, automatically sets the <modal> property to YES, which enables user interactivity in the HUD.
+*/
+@property (nonatomic, strong) void (^chromeTapActionBlock) ();
 
 /**
 * Indicates whether the view controller should be dismissed or not when the user taps the HUD's overlay view.
@@ -80,24 +88,64 @@
 * Setting this to YES, automatically sets the <modal> property to YES, which enables user interactivity in the HUD.
 */
 @property (nonatomic) BOOL shouldDismissOnOverlayTap;
+
+/**
+* Block to execute when the user taps the HUD's overlay view.
+*
+* Setting this to a value other than nil, automatically sets the <modal> property to YES, which enables user interactivity in the HUD.
+*/
 @property (nonatomic, strong) void (^overlayTapActionBlock) ();
 
+/**
+* Indicates whether the HUD will modal or not.
+* When modal, the user will not be able to interact with the views behind the HUD view.
+* Default: YES.
+*/
 @property (nonatomic) BOOL modal;
 
+/**
+* Delay, in seconds, after which the HUD view is dismissed automatically.
+* This property has no effect when set to 0.
+* Default: 0
+*/
 @property (nonatomic) NSTimeInterval autoDismissalDelay;
 
 /**
 * Duration (in seconds) of the presentation's transition animation.
+* If set to 0, then no animation occurs.
 */
 @property(nonatomic) NSTimeInterval presentationAnimationDuration;
 
 /**
 * Duration (in seconds) of the dismissal's transition animation.
+* If set to 0, then no animation occurs.
 */
 @property(nonatomic) NSTimeInterval dismissalAnimationDuration;
 
+/**
+* Present the HUD view controller.
+*
+* @param a_parentViewController Parent view controller to which the receiver will be added as a child view controller.
+* If nil, a dedicated UIWindow instance with a UIViewController instance as its root view controller will be created.
+* The receiver will then be added as a child view controller of the dedicated UIWindow's root view controller.
+*
+* @param a_parentView Parent view controller's view to which the receiver's view will be added as a subview.
+* If nil, a_parentViewController's view is used as the parent view.
+*
+* @param a_animated Indicates whether the presentation transition will be animated or not. The <presentationAnimationDuration> property determines the duration of the animation.
+*
+* @param a_completion Block to execute after the presentation transition completes.
+*/
 - (void)presentHudViewControllerWithParentViewController:(UIViewController *)a_parentViewController
                                               parentView:(UIView *)a_parentView animated:(BOOL)a_animated completion:(void (^)(BOOL a_finished))a_completion;
+
+/**
+* Dismiss the HUD view controller.
+*
+* @param a_animated Indicates whether the dismissal transition will be animated or not. The <dismissalAnimationDuration> property determines the duration of the animation.
+*
+* @param a_completion Block to execute after the dismissal transition completes.
+*/
 - (void)dismissHudViewControllerWithAnimated:(BOOL)a_animated completion:(void (^)(BOOL a_finished))a_completion;
 
 @end
