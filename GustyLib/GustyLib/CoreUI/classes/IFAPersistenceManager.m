dadbf2d28aed818ccb3fbfce9d2244c537520fb5
Marcelo Schroeder <marcelo.schroeder@infoaccent.com>
2015-03-12 10:14:52 +1100

    IMPROVEMENT - System entities now honour the "listSortProperties" entity configuration setting (it overrides the default sort order for system entities).

--- GustyLib/GustyLib/CoreUI/classes/IFAPersistenceManager.m
+++ GustyLib/GustyLib/CoreUI/classes/IFAPersistenceManager.m
@@ -954,33 +954,37 @@ static NSString *METADATA_KEY_SYSTEM_DB_TABLES_VERSION = @"systemDbTablesVersion
 - (NSArray*)listSortDescriptorsForEntity:(NSString*)anEntityName{
 	NSMutableArray *sortDescriptors = [NSMutableArray array];
 	NSSortDescriptor *sortDescriptor;
-	if ([self isSystemEntityForEntity:anEntityName]) {
-		sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"index" ascending:YES];
-		[sortDescriptors addObject:sortDescriptor];
-        sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"name" ascending:YES];  // fallback to name
-        [sortDescriptors addObject:sortDescriptor];
-	}else if ([self.entityConfig listReorderAllowedForEntity:anEntityName]) {
+    if ([self.entityConfig listReorderAllowedForEntity:anEntityName]) {
 		// The list's sort order is controlled by the user (if provided, entity config's listSortProperties is ignored)
 		sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"seq" ascending:YES];
 		[sortDescriptors addObject:sortDescriptor];
 	}else {
-		// The list's sort order is dictated by the configuration
-		for(NSDictionary* sortItem in [self.entityConfig listSortPropertiesForEntity:anEntityName]){
-            NSString *keyPath = [sortItem objectForKey:@"name"];
-//            NSLog(@"keyPath: %@", keyPath);
-            BOOL ascending = [[sortItem objectForKey:@"ascending"] boolValue];
-            Class l_propertyClass = [IFAUtils classForPropertyNamed:keyPath inClassNamed:anEntityName];
-//            NSLog(@"l_propertyClass: %@", [l_propertyClass description]);
-			if ([l_propertyClass isSubclassOfClass:[NSString class]]) {
-				sortDescriptor = [[NSSortDescriptor alloc] initWithKey:keyPath ascending:ascending selector:NSSelectorFromString(@"localizedCaseInsensitiveCompare:")];
-//                NSLog(@"  USING localised case insensitive compare...");
-			}else {
-				sortDescriptor = [[NSSortDescriptor alloc] initWithKey:keyPath ascending:ascending];
-//                NSLog(@"  NOT using localised case insensitive compare...");
-			}
-			[sortDescriptors addObject:sortDescriptor];
-		}
-	}
+        NSArray *listSortProperties = [self.entityConfig listSortPropertiesForEntity:anEntityName];
+        if (listSortProperties.count) {
+            // The list's sort order is dictated by the configuration
+            for(NSDictionary* sortItem in listSortProperties){
+                NSString *keyPath = [sortItem objectForKey:@"name"];
+                //            NSLog(@"keyPath: %@", keyPath);
+                BOOL ascending = [[sortItem objectForKey:@"ascending"] boolValue];
+                Class l_propertyClass = [IFAUtils classForPropertyNamed:keyPath inClassNamed:anEntityName];
+                //            NSLog(@"l_propertyClass: %@", [l_propertyClass description]);
+                if ([l_propertyClass isSubclassOfClass:[NSString class]]) {
+                    sortDescriptor = [[NSSortDescriptor alloc] initWithKey:keyPath ascending:ascending selector:NSSelectorFromString(@"localizedCaseInsensitiveCompare:")];
+                    //                NSLog(@"  USING localised case insensitive compare...");
+                }else {
+                    sortDescriptor = [[NSSortDescriptor alloc] initWithKey:keyPath ascending:ascending];
+                    //                NSLog(@"  NOT using localised case insensitive compare...");
+                }
+                [sortDescriptors addObject:sortDescriptor];
+            }
+        } else if ([self isSystemEntityForEntity:anEntityName]) {
+            // Falls back to the default sort order for system entities
+            sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"index" ascending:YES];
+            [sortDescriptors addObject:sortDescriptor];
+            sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"name" ascending:YES];  // fallback to name
+            [sortDescriptors addObject:sortDescriptor];
+        }
+    }
 	return sortDescriptors;
 }
 
